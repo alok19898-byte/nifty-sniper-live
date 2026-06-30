62e67ef846b944649efb0d0d9c5273510efb714b
alok19898-byte <alok19898@gmail.com>
2026-06-30 11:53:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {
-        "request": request, 
-        "message": "Bhai tera Backend aur Frontend dono connect ho gaye!",
-        "status": "Live & Running"
-    })
+    return templates.TemplateResponse(
+        "index.html", 
+        {
+            "request": request, 
+            "message": "Bhai tera Backend aur Frontend dono connect ho gaye!", 
+            "status": "Live & Running"
+        }
+    )
