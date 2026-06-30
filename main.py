71d24c211db2e17548ca0d29bcbd1c00c9413021
alok19898-byte <alok19898@gmail.com>
2026-06-30 12:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+mport random
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -7,8 +8,13 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
+    # यहाँ हम रैंडम निफ्टी भाव जनरेट कर रहे हैं
+    random_price = round(random.uniform(23000, 24000), 2)
     return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={"message": "Bhai tera Backend aur Frontend dono connect ho gaye!", "status": "Live & Running"}
+        "index.html", 
+        {
+            "request": request, 
+            "price": random_price,
+            "status": "Live & Running"
+        }
     )
