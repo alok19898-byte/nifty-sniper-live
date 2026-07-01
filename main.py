7823b5218f5a249b86826078f181690c27cbad17
alok19898-byte <alok19898@gmail.com>
2026-07-01 08:24:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# 1. यह लाइन सबसे ज़रूरी है! यह सर्वर को static फोल्डर के बारे में बताती है
+# यह लाइन सर्वर को static फोल्डर के बारे में बताती है
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# 2. यह लाइन templates (HTML) फोल्डर के बारे में बताती है
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})r:</h2><p>{str(e)}</p>")
+    return templates.TemplateResponse("index.html", {"request": request})
